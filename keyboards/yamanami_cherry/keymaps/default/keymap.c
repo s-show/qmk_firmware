0ef5c747100cf02ef9a394e4c7101a6ace4ae622
s-show <s1shimz@gmail.com>
2025-02-22 09:53:15 +0000

    update keymap (tap_dance SCLN to COLN).

--- keyboards/yamanami_cherry/keymaps/default/keymap.c
+++ keyboards/yamanami_cherry/keymaps/default/keymap.c
@@ -203,9 +203,11 @@ const key_override_t *key_overrides[] = {
 // tap dance setting start
 enum {
     TD_ALT_F4,
+    TD_SC_CL,
 };
 tap_dance_action_t tap_dance_actions[] = {
-    [TD_ALT_F4] = ACTION_TAP_DANCE_DOUBLE(KC_NO, LALT(KC_F4))
+    [TD_ALT_F4] = ACTION_TAP_DANCE_DOUBLE(KC_NO, LALT(KC_F4)),
+    [TD_SC_CL] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, LSFT(KC_SCLN))
 };
 // tap dance setting end
 
@@ -214,7 +216,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
     // Keycap is US, OS layout is US.
     [_QWERTY_US_US] = LAYOUT(
         KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,               KC_T,          KC_Y,    KC_U,             KC_I,    KC_O,    KC_P,    KC_GRV,  \
-        KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,               KC_G,          KC_H,    KC_J,             KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+        KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,               KC_G,          KC_H,    KC_J,             KC_K,    KC_L,    TD(TD_SC_CL), KC_QUOT, \
         KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,               KC_B,          KC_N,    KC_M,             KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
         WIN_TAB, KC_ESC, KC_LALT, KC_LGUI, MO(_LOWER_US_US),   SFT_T(KC_SPC), KC_BSPC, MO(_RAISE_US_US), KC_RGUI, KC_RALT, KC_RCTL, ALT_TAB \
     ),
